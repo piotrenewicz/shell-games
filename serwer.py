04bbc9cd0c6167d6742d2976ddb26958024b2483
Piotrenewicz <piotrekm9@onet.eu>
2021-12-08 23:34:15 +0100

    Server upgrade, now non-blocking, it can check both clients, and get messages between out of dialog order! It also can shovel long transmissions, in parts. It has no idea what is in the transmission, or where it ends. So clients will need to work out the protocol details

--- serwer.py
+++ serwer.py
@@ -1,4 +1,5 @@
 import socket
+import time
 
 host = "127.0.0.1"
 port = 1109
@@ -7,30 +8,54 @@ game_state = None
 
 def serwer_loop(player1, player2):
     serwer_running = True
-    try:
-        while serwer_running:
-            player2, player1 = player1, player2
+    print("starting data transfer between players!")
+    while serwer_running:
+        time.sleep(.1)
+        player2, player1 = player1, player2  # exchange players to alternate attempts to push data through
+        try:
             msg = player1.recv(1024)
-            print(msg)
-            player2.sendall(msg)
-    except:
-        pass
+        except socket.timeout as e:
+            err = e.args[0]
+            # this next if/else is a bit redundant, but illustrates how the
+            # timeout exception is setup
+            if err == 'timed out':
+                continue
+            else:
+                print(e)
+                serwer_running = False
+                print("we have a timeout problem")
+                continue
+        except socket.error as e:
+            print(e)
+            serwer_running = False
+            print("error on the socket")
+            continue
+        else:
+            if len(msg) == 0:
+                print('Got an empty transmission, this means client disconnected.')
+                serwer_running = False
+                continue
+            else:
+                # got a message do something :)
+                player2.send(msg)
+    print("server closing")
 
 
 def begin_serwer():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
+        print("server started, waiting for clients")
         player1, p1addr = s.accept()
-        with player1:
-            player1.sendall(b'1')
-            game_state = player1.recv(1024)
-            print(game_state)
-            player2, p2addr = s.accept()
-            with player2:
-                player2.sendall(b'2')
-                player2.sendall(game_state)
-                serwer_loop(player1, player2)
+        player1.setblocking(False)
+        player1.send(b'1')
+        print("client1 connected.", p1addr)
+        player2, p2addr = s.accept()
+        player2.setblocking(False)
+        player2.send(b'2')
+        print("client2 connected.", p2addr)
+        serwer_loop(player1, player2)
+        s.close()  # server loop has quit, closing shop.
 
 
 if __name__ == '__main__':

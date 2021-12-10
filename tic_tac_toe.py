ca6ef65ece4ade5c10667f8c994656b8f23bed83
Piotr Morel <35681417+piotrenewicz@users.noreply.github.com>
2021-12-10 11:44:44 +0100

    Adapting TicTacToe to server code.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,23 @@
 from aux.aux_tic_tac_toe import *
 import curses
 
+# =====added here during merge to keep this code compatible with server=======
+import client
+
+class TicTacToeState(client.GameState):
+    def __init__(self):
+        self.game_id = 2
+
+def create_game(window: curses.window):
+    client.start_connection('127.0.0.1', 1109)
+    client.game = TicTacToeState()
+    client.send_game(client.game)
+    main(window)
+    
+def join_game(window: curses.window):
+    main(window)
+# ============================================================================
+    
 
 def welcome(window: curses.window):
     lines = ["Welcome to Tic-Tac-Toe game!",
